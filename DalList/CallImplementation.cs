4783f2d4977b2763c28efce5061a30c07184352d
Malka Haupt <nkfv00@gmail.com>
2024-11-12 18:20:29 +0200

    ccont stage1

--- DalList/CallImplementation.cs
+++ DalList/CallImplementation.cs
@@ -1,37 +1,76 @@
-﻿
-
-namespace Dal;
+﻿namespace Dal;
 using DO;
 using DalApi;
+
 public class CallImplementation : ICall
 {
+    private List<Call> calls = new List<Call>();
+
     public void Create(Call item)
     {
-        throw new NotImplementedException();
+        // יצירת מזהה חדש על בסיס המספר הרץ הבא
+        int newId = Config.NextCallId;
+
+        
+        Call newItem = new Call 
+        {
+            Id = newId,
+            
+             Type=item.Type,
+             Description=item.Description,
+             FullAddress=item.FullAddress,
+             Latitude=item.Latitude,
+             Longitude=item.Longitude,
+             TimeOpened = item.TimeOpened,
+             MaxTimeToClose = item.MaxTimeToClose    
+         
+        };
+        if (calls.Exists(c => c.Id == item.Id))
+        {
+            throw new ArgumentException("Call with the same ID already exists.");
+        }
+        calls.Add(item);
+     //   return newItem.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Call call = calls.Find(c => c.Id == id)
+            ?? throw new KeyNotFoundException("Call not found.");
+        calls.Remove(call);
     }
 
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        calls.Clear();
     }
 
     public Call? Read(int id)
     {
-        throw new NotImplementedException();
+        return calls.Find(c => c.Id == id)
+            ?? throw new KeyNotFoundException("Call not found.");
     }
 
     public List<Call> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Call>(calls);
     }
 
     public void Update(Call item)
     {
-        throw new NotImplementedException();
+        int index = calls.FindIndex(c => c.Id == item.Id);
+        if (index == -1)
+        {
+            throw new KeyNotFoundException("Call not found.");
+        }
+         calls[index] = item;
+
+        // הסרה של האובייקט הישן
+       // calls.RemoveAt(index);
+
+        // הוספה של האובייקט המעודכן
+      //  calls.Add(item);
+
+
     }
 }
