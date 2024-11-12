a4c0cac79d72f3f32194dc1a5b457a8f2debd7a3
Malka Haupt <nkfv00@gmail.com>
2024-11-12 09:28:06 +0200

    cont3 stage1

--- DalFacade/DO/Assignment.cs
+++ DalFacade/DO/Assignment.cs
@@ -1,13 +1,14 @@
 ﻿namespace DO;
 public record Assignment
-{
-     int Id { get; set; }             // מזהה ייחודי להקצאה
-     int CallId { get; set; }         // מזהה הקריאה שהמתנדב בחר לטפל בה
-   int VolunteerId { get; set; }    // ת.ז של המתנדב שבחר לטפל בקריאה
-    DateTime TimeStart { get; set; } // זמן כניסה לטיפול (תאריך ושעה)
-     DateTime? TimeEnd = null;  // זמן סיום הטיפול בפועל (תאריך ושעה)
-     TypeEnd? TypeEndTreat = null; // סוג סיום הטיפול (טופלה, ביטול עצמי, ביטול מנהל, ביטול פג תוקף)
+(
+    int Id,           // מזהה ייחודי להקצאה
+    int CallId,        // מזהה הקריאה שהמתנדב בחר לטפל בה
+    int VolunteerId,   // ת.ז של המתנדב שבחר לטפל בקריאה
+    DateTime TimeStart, /// זמן כניסה לטיפול (תאריך ושעה)
+   DateTime? TimeEnd = null, // זמן סיום הטיפול בפועל (תאריך ושעה)
+   TypeEnd? TypeEndTreat = null// סוג סיום הטיפול (טופלה, ביטול עצמי, ביטול מנהל, ביטול פג תוקף)
+)
+{ public Assignment() : this(0, 0, 0, default(DateTime)) { } }
 
-   public Assignment() : this(default(DateTime)) { }
 
-}
+
--- DalFacade/DO/Volunteer.cs
+++ DalFacade/DO/Volunteer.cs
@@ -1,30 +1,32 @@
 ﻿
-
-
-
 namespace DO;
 
-public record class Volunteer
+public record Volunteer
+(
+   
+     int Id,
+     string FullName,
+     string PhoneNumber,
+     string Email,
+       Distance TypeDistance,
+     Role Job,
+     bool Active,
+     string? Password = null,
+     string? FullAddress = null,
+     double? Latitude = null,
+     double? Longitude = null,
+     double? MaxReading = null
+    
+)
 {
-    public int Id { get; set; }                  // מזהה ייחודי של המתנדב
-    public string FullName { get; set; }         // שם מלא (שם פרטי ושם משפחה)
-    public string PhoneNumber { get; set; }      // מספר טלפון
-    public string Email { get; set; }            // כתובת דוא"ל
-    public string? Password { get; set; }        // סיסמה (אם רלוונטי)
-    public string? FullAddress { get; set; }     // כתובת מלאה
-    public double? Latitude { get; set; }        // קו רוחב
-    public double? Longitude { get; set; }       // קו אורך
-    public Role Job { get; set; }                // תפקיד (מתנדב או מנהל)
-    public bool Active { get; set; }             // האם המשתמש פעיל
-    public double? MaxReading { get; set; }      // מרחק מקסימלי לפעילות
-    public Distance TypeDistance { get; set; }   // סוג המרחק (אווירי, הליכה, נסיעה)
-
     /// <summary>
     /// Default constructor for stage 3
     /// </summary>
-    public Volunteer() : this(){ }
-
-
-    // פונקציה לעדכון מיקום המתנדב
-  
+    public Volunteer() : this(0, "", "", "", default(Role), default(Distance), false) { }
 }
+
+
+
+
+  
+  
