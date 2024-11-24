56849ded1d99b712bacf5a5e11b30cbca8528a2e
Malka Haupt <nkfv00@gmail.com>
2024-11-24 19:08:47 +0200

    satge3 part1

--- DalXml/VolunteerImplementation.cs
+++ DalXml/VolunteerImplementation.cs
@@ -2,6 +2,8 @@
 
 using DalApi;
 using DO;
+using System;
+using System.Xml.Linq;
 
 namespace Dal;
 
@@ -22,18 +24,55 @@ internal class VolunteerImplementation : IVolunteer
         throw new NotImplementedException();
     }
 
+
+
+
+
+
+           static Volunteer getStudent(XElement s)
+        {
+            return new DO.Volunteer()
+            {
+                Id = s.ToIntNullable("Id") ?? throw new FormatException("can't convert id"),
+                FullName = (string?)s.Element("Name") ?? "",
+                Email = (string?)s.Element("Email") ?? "",
+                PhoneNumber = (string?)s.Element("PhoneNumber") ?? "",
+               // TypeDistance = Enum.TryParse<Distance>((string?)s.Element("TypeDistance"), true, out var typeDistance) ? typeDistance : (Distance?)null,
+               // Job = Enum.TryParse<Role>((string?)s.Element("Job"), true, out var job) ? job : (Role?)null,
+                Active = (bool?)s.Element("IsActive") ?? false,
+                Password = (string?)s.Element("Password"),
+                FullAddress = (string?)s.Element("FullAddress"),
+                Latitude = (double?)s.Element("Latitude"),
+                Longitude = (double?)s.Element("Longitude"),
+                MaxReading = (double?)s.Element("MaxReading")
+            };
+        }
+
+   
+
     public Volunteer? Read(int id)
     {
-        throw new NotImplementedException();
+        XElement? studentElem =
+    XMLTools.LoadListFromXMLElement(Config.s_volunteers_xml).Elements().FirstOrDefault(st => (int?)st.Element("Id") == id);
+        return studentElem is null ? null : getStudent(studentElem);
     }
 
-    public IEnumerable<Volunteer> ReadAll(Func<Volunteer, bool>? filter = null)
+    public Volunteer? Read(Func<Volunteer, bool> filter)
     {
-        throw new NotImplementedException();
+        return XMLTools.LoadListFromXMLElement(Config.s_volunteers_xml).Elements().Select(s => getStudent(s)).FirstOrDefault(filter);
     }
 
     public void Update(Volunteer item)
     {
-        throw new NotImplementedException();
+        XElement studentsRootElem = XMLTools.LoadListFromXMLElement(Config.s_volunteers_xml);
+
+        (studentsRootElem.Elements().FirstOrDefault(st => (int?)st.Element("Id") == item.Id)
+        ?? throw new DO.DalDeletImposible($"Volunteer with ID={item.Id} does Not exist"))
+                .Remove();
+
+        studentsRootElem.Add(new XElement("Volunteer", createStudentElement(item)));
+
+        XMLTools.SaveListToXMLElement(studentsRootElem, Config.s_volunteers_xml);
     }
+
 }
