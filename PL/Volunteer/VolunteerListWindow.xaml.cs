39181a9a526c885a8e269e1954e599a8264e9a4e
Malka Haupt <nkfv00@gmail.com>
2024-12-24 13:26:59 +0200

    stage5/7/1

--- PL/Volunteer/VolunteerListWindow.xaml.cs
+++ PL/Volunteer/VolunteerListWindow.xaml.cs
@@ -19,9 +19,28 @@ namespace PL.Volunteer
     /// </summary>
     public partial class VolunteerListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        public IEnumerable<BO.VolunteerInList> VolunteerList
+        {
+            get { return (IEnumerable<BO.VolunteerInList>)GetValue(VolunteerListProperty); }
+            set { SetValue(VolunteerListProperty, value); }
+        }
+
+        public static readonly DependencyProperty VolunteerListProperty =
+            DependencyProperty.Register("VolunteerList", typeof(IEnumerable<BO.VolunteerInList>), typeof(VolunteerListWindow), new PropertyMetadata(null));
+
+
+
+
         public VolunteerListWindow()
         {
             InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
