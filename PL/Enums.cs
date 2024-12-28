27010c7495e075cc3d0a8c78cbacf6be78ef6a83
Malka Haupt <nkfv00@gmail.com>
2024-12-28 20:21:03 +0200

    stage5/8

---
+++ PL/Enums.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace PL;
+
+internal class VolunteersCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.EVolunteerInList> s_enums =
+        (Enum.GetValues(typeof(BO.EVolunteerInList)) as IEnumerable<BO.EVolunteerInList>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
+
