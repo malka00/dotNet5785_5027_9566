b2026783df961aa5972465134fb683c9d472cd0d
Malka Haupt <nkfv00@gmail.com>
2025-01-05 16:26:16 +0200

    stage6.1

---
+++ PL/EnterWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using PL.Volunteer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+       
+
+
+        public EnterWindow()
+        {
+           
+            InitializeComponent();
+
+
+        }
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+           
+        
+            
+
+            try
+            {
+              BO.Volunteer  currentVolunteer =  s_bl.Volunteers.Read(id);
+            }
+            catch (BO.BlDoesNotExistException ex)
+            {
+               
+                MessageBox.Show(ex.Message, "Operation Fail", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Operation Fail", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            if(currentVolunteer.Password!=password)
+                MessageBox.Show("wrong password!","Error", MessageBoxButton.OK);
+
+            else
+            {
+                if(currentVolunteer.Job==BO.Role.Volunteer)
+               
+            }
+
+        }
+    }
+}
