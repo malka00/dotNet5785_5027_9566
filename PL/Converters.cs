2a7855250ae38d1836bdf4d6ae5b13aca0b6bfff
אפרת שרעבי <efsharab@g.jct.ac.il>
2025-02-01 20:15:35 +0200

    stage 7- cinverters

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -12,7 +12,9 @@ using BO;
 
 namespace PL
 {
-    //convert for null courdinats
+    /// <summary>
+    /// convert for null courdinats
+    /// </summary>
     public class ConverterThreeValueNullToColor : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
@@ -38,6 +40,10 @@ namespace PL
 
     }
 
+    /// <summary>
+    /// The function checks whether the volunteer has no active call and is active.If so - returns true, otherwise false.
+    /// Linked to the call selection button
+    /// </summary>
     public class BoolConvertIsCallInProsses : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -63,6 +69,11 @@ namespace PL
         }
     }
 
+    /// <summary>
+    /// Checking whether it is possible to delete a call (CanDelete)
+    /// If so → returns Visibility.Visible(the element will be visible).
+    /// If not → returns Visibility.Collapsed(the element is hidden).
+    /// </summary>
     public class ConvertIsCanDeletCall : IValueConverter
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
@@ -81,6 +92,11 @@ namespace PL
         }
     }
 
+    /// <summary>
+    /// Checking whether it is possible to delete a volunteer (CanDelete)
+    /// If so → returns Visibility.Visible(the element will be visible).
+    /// If not → returns Visibility.Collapsed(the element is hidden).
+    /// </summary>
     public class ConvertIsCanDeletVolunteer : IValueConverter
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
@@ -99,6 +115,9 @@ namespace PL
         }
     }
 
+    /// <summary>
+    /// Checks whether the volunteer currently has a call in his care and if so returns Visibility.Visible (the element is displayed)
+    /// </summary>
     public class ConvertIsCallInProsses : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -115,6 +134,10 @@ namespace PL
             throw new NotImplementedException();
         }
     }
+
+    /// <summary>
+    /// Checks whether the user is in voluntary update mode, and if so, returns true - that is, the button for changing the id will be read-only
+    /// </summary>
     public class ConvertUpdateToTrue : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -132,55 +155,53 @@ namespace PL
         }
     }
 
+  
+    //public class ConvertUpdateToVisible : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        if (value is string buttonText)
+    //        {
+    //            return buttonText == "Update" ? Visibility.Visible : Visibility.Collapsed;
+    //        }
+    //        return Visibility.Collapsed;
+    //    }
 
-    public class ConvertUpdateToVisible : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is string buttonText)
-            {
-                return buttonText == "Update" ? Visibility.Visible : Visibility.Collapsed;
-            }
-            return Visibility.Collapsed;
-        }
+    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
+   
 
-
-
-    public class BooleanToVisibility : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool boolValue && boolValue)
-            {
-                return Visibility.Visible;
-            }
-            return Visibility.Collapsed;
-        }
-
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-
-            if (value is Visibility visibility && visibility == Visibility.Visible)
-            {
-                return true;
-            }
-            return false;
-        }
-
-    }
+    //public class BooleanToVisibility : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        if (value is bool boolValue && boolValue)
+    //        {
+    //            return Visibility.Visible;
+    //        }
+    //        return Visibility.Collapsed;
+    //    }
 
 
+    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
 
+    //        if (value is Visibility visibility && visibility == Visibility.Visible)
+    //        {
+    //            return true;
+    //        }
+    //        return false;
+    //    }
 
+    //}
 
-
+    /// <summary>
+    /// A function that is used to return a collection of values ​​from an Enum (counter class) based on a value selected for sorting ("Status" or "CallType").
+    /// </summary>
     public class ConvertStatusOrTypeToEnum : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -205,6 +226,10 @@ namespace PL
         }
     }
 
+    /// <summary>
+    /// A function that converts a boolean value to a string that will be displayed on the button. 
+    /// Linked to the IsSimulatorRunning attribute
+    /// </summary>
     public class ConverterSimulatorState : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -217,6 +242,13 @@ namespace PL
             throw new NotImplementedException();
         }
     }
+
+    /// <summary>
+    /// A function that operates on a boolean value and returns its opposite value.
+    /// If the value is true, the function returns false.
+    /// If the value is false, the function returns true
+    /// Linked to the IsSimulatorRunning attribute to know whether to allow clicking on the other buttons
+    /// </summary>
     public class InverseBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -225,31 +257,12 @@ namespace PL
             {
                 return !booleanValue;
             }
-            return false; // ערך ברירת מחדל במקרה שהקלט אינו בוליאני
+            return false; 
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException("ConvertBack לא נתמך.");
-        }
-    }
-
-
-
-    public class InverseBooleanConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool boolValue)
-            {
-                return !boolValue; // הפיכת הערך הבוליאני
-            }
-            return false; // ברירת מחדל במקרה של שגיאה
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException(); // לא נחוץ במקרה הזה
         }
     }
 }
